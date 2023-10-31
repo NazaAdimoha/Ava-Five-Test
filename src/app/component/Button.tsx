c2da4fd1bda11404bf7aba4361da44c8a81b0cc1
nazaadimoha <nazaadimoha98@gmail.com>
2023-10-31 17:07:16 +0100

    create custom button component

--- src/app/component/Button.tsx
+++ src/app/component/Button.tsx
@@ -1,9 +1,51 @@
+"use client";
 
-
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+interface ButtonProps {
+  variant: "primary" | "secondary" | "tertiary";
+  size: "small" | "medium" | "large";
+  onClick: () => void;
+  disabled?: boolean;
+  icon?: React.ReactNode;
+  children: React.ReactNode;
 }
 
-export default Button
+const Button = ({
+  variant,
+  size,
+  onClick,
+  disabled,
+  icon,
+  children,
+}: ButtonProps) => {
+  const variantClasses = {
+    primary: "bg-primary text-white",
+    secondary: "bg-secondary text-white",
+    tertiary: "bg-transparent text-gray-500",
+  };
+
+  const sizeClasses = {
+    small: "px-4 py-1 text-sm",
+    medium: "px-6 py-2 text-base",
+    large: "px-8 py-3 text-lg",
+  };
+
+  return (
+    <button 
+        className={`
+            ${variantClasses[variant]} 
+            ${sizeClasses[size]} 
+            ${disabled ? "opacity-50 cursor-not-allowed" : ""}
+            flex items-center justify-center rounded-md space-x-2
+        `}
+        onClick={onClick}
+        disabled={disabled}
+    >
+        {
+            icon && <span className="">{icon}</span>
+        }
+        <span>{children}</span>
+    </button>
+  );
+};
+
+export default Button;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      //define custom colors for buttons
+      colors: {
+        primary: "#1B4B66",
+        secondary: "#fff",
+        tertiary: "#B00020",
+      }
     },
   },
   plugins: [],
