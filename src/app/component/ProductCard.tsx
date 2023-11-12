1a175dda5ba16233b6e9fff1ed7533a7d1018a69
nazaadimoha <nazaadimoha98@gmail.com>
2023-11-12 04:04:43 +0100

    bug-fix: product not rendering

--- src/app/component/ProductCard.tsx
+++ src/app/component/ProductCard.tsx
@@ -4,7 +4,7 @@ interface ProductCardProps {
   title: string;
   price: number;
   description: string;
-  image: string;
+  image: string[];
   isFavourite?: boolean;
 };
 
@@ -16,8 +16,10 @@ const ProductCard = ({
   isFavourite
 }:ProductCardProps) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 sm:p-2 md:p-4 lg:p-6">
-      <Image src={image} alt={title} className="w-full h-48 object-cover rounded-t-lg mb-4" />
+    <div className="bg-primary gap-4 rounded-lg shadow-md p-4 sm:p-2 md:p-4 lg:p-6">
+      <Image src={
+        image[0]
+      } alt={title} width={30} height={30} className="object-cover rounded-lg mb-4" />
       <div className="p-2">
         <div className="flex justify-between items-center">
           <h3 className="text-lg sm:text-base md:text-lg lg:text-xl font-semibold">{isFavourite}</h3>
--- src/app/component/ProductList.tsx
+++ src/app/component/ProductList.tsx
@@ -3,22 +3,53 @@ import Link from "next/link";
 import { useFetchProducts } from "../hooks/useFetchProduct";
 import CaretRight from "../../../public/chevron-right.svg";
 import Image from "next/image";
+import ProductCard from "./ProductCard";
+import { Key } from "react";
 
 const ProductList = () => {
   const { products, isLoading, error } = useFetchProducts();
+
+  //define product state
+  const productList = products;
   return (
-    <section className="bg-secondary">
+    <section className="bg-secondary p-10">
       <div className="flex items-center justify-between">
         <p>New Arrivals</p>
         <div>
-          <Link href="/product" className="p-[10px 8px] flex justify-center items-center gap-4 border rounded-sm">
-            View All <span>
-                <Image src={CaretRight} alt="caretDown" width={20} height={20} />
+          <Link
+            href="/product"
+            className="p-[10px] text-primary flex justify-center items-center gap-4 border rounded-md transition ease-in-out delay-150 bg-slate-400 hover:-translate-y-1 hover:scale-110 hover:bg-primary hover:text-secondaryText duration-300 "
+          >
+            View All{" "}
+            <span>
+              <Image src={CaretRight} alt="caretDown" width={20} height={20} />
             </span>
           </Link>
         </div>
       </div>
-      <div></div>
+      {/* product list */}
+
+      <div className="gap-4 grid grid-rows-2 md:grid-rows-4 ">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Error fetching product</div>
+        ) : (
+          productList && productList.products.map(
+            (product: { title: string; price: number; description: string; images: string; isFavourite: boolean | undefined; }, index: Key | null | undefined) => (
+            <ProductCard
+                key={index}
+                title={product.title}
+                price={product.price}
+                description={product.description}
+                image={
+                    [product.images[0]]
+                }
+                isFavourite={product.isFavourite}
+            />
+          )
+        ))}
+      </div>
     </section>
   );
 };
